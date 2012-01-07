a71ed8093a8d2580222dc77c354433cd730d9f2a
John West <john.sheffield.west@gmail.com>
2012-01-07 12:32:54 -0500

    scaffolds. yay.

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,9 @@ module Project
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Scaffold with SASS instead of SCSS
+    config.sass.preferred_syntax = :sass
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Project::Application.routes.draw do
+  resources :shared_articles
+
+  resources :articles
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
