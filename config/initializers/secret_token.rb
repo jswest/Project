521f8da3a91f2b83784a5e2b787a97b69f0123fc
John West <john.sheffield.west@gmail.com>
2012-01-07 10:52:58 -0500

    first commit: changed gemfile to add haml and changed config/application.ru to add sass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '7c2671a11df8748edde0c85e8d78b646f17e7c3b892a943ce8d0e1ab2e69136c877e170648558fddfb3e168ea39c77760fc12bb5ed563c90927fdb3c9c23ee7f'
