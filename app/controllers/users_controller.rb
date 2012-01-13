152d61e899800cb7d9fae9064225d1a24e8daa45
John West <john.sheffield.west@gmail.com>
2012-01-13 13:31:30 -0500

    broken ajax on fp and some not-so-good code in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
+
+
+  def generate_front_page_articles
+    @front_page_articles = []
+    @received_articles = @user.received_articles.limit(3)
+    if !@received_articles.empty?
+      @received_articles.each do |received_article|
+        @front_page_articles.push( {
+          :shared_by => received_article.shared_by.firstname,
+          :article => received_article.article,
+          :classes => "tall shared"
+        } )
+      end
+    end
+    nytimes_articles = Article.where( :first_paragraph.present? )
+    while @front_page_articles.length < 7 do
+      article = nytimes_articles.sample
+      @front_page_articles.push( {
+        :shared_by => "The New York Times", 
+        :article => article,
+        :classes => "tall nytimes tall-nytimes"
+      } )
+    end
+
+    return @front_page_articles   
+      
+  end
+
+
+
+
+
   
   # force_ssl
   def index
@@ -28,37 +60,7 @@ class UsersController < ApplicationController
       @user = User.find( params[:id] )
     end
     
-    classes = [
-      "venti",
-      "tall",
-      "tall",
-      "grande-vertical",
-      "grande-horizonal",
-      "tall",
-      "tall"
-    ]
-    
-    @front_page_articles = []
-    @received_articles = @user.received_articles.limit(3)
-    
-    @received_articles.each do |received_article|
-      @front_page_articles.push( {
-        :shared_by => received_article.shared_by.firstname,
-        :article => received_article.article,
-        :classes => "tall shared"
-      } )
-    end
-    
-    while @front_page_articles.length < 7 do
-      @front_page_articles.push( {
-        :shared_by => "The New York Times", 
-        :article => Article.where( :first_paragraph.present? ).sample,
-        :classes => "tall nytimes tall-nytimes"
-      } )
-    end
-    
-    
-    
+    @front_page_articles = generate_front_page_articles
 
     respond_to do |format|
       format.html
@@ -66,8 +68,18 @@ class UsersController < ApplicationController
     end
     
   end
+    
+  def update_front_page
+    deleted_article = Article.find( params[:id] )
+    @front_page_articles.delete( deleted_article )
+    @front_page_articles.push( {
+      :shared_by => "The New York Times",
+      :article => Article.where( :first_paragraph.present? ).sample,
+      :classes => "tall nytimes tall-nytimes"
+    } )
+    render :partial => "front_page", :collection => @front_page_articles, :as => :article
+  end
   
-
 
   def new
     
