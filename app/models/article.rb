213326126fbaa377cf9a6d9a3b7b857f2b38bb83
Gabe Isman <gabe.isman@gmail.com>
2012-01-13 17:56:12 -0500

    Refactored user_controller into front_page_controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,21 @@ class Article < ActiveRecord::Base
 
   #validates_presence_of :title
   validates_presence_of :url
+  attr_accessible :classes
   
   belongs_to :user
+
+  # CSS classes for displaying the article on the frontpage
+  def self.classes
+  [
+    "venti",
+    "tall",
+    "tall",
+    "grande-vertical",
+    "grande-horizonal",
+    "tall",
+    "tall"
+  ]
+  end
 
 end
